9895dd0be7ebfd86a4e137c3609891ff4b587af9
winfan <121552591@qq.com>
2016-09-23 11:47:27 +0800

    begin 30 days trip

---
+++ index.ios.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const {AppRegistry} = require('react-native');
+const Playground = require('./js/Playground');
+
+AppRegistry.registerComponent('ThirtyDaysOfReactNative',() => Playground);
